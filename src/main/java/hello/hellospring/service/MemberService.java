6734d3b555e00255c566b8acae701463d3c5d069
zmfl1230 <zmfl123097@gmail.com>
2022-01-04 17:51:16 +0900

    feat: develop member service

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,33 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public void join (String name) {
+        // 중복 이름 방지
+        validateDuplicate(name);
+        memberRepository.save(name);
+    }
+
+    private void validateDuplicate(String name) {
+        memberRepository.findByName(name)
+                .ifPresent(member -> {
+                    throw new IllegalStateException("이미 존재하는 이름입니다. " + member);
+                });
+    }
+
+    public List<Member> findMembers () {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
