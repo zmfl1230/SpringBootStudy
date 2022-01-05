e17a1af6ddb8a89feb5b98ef234782a9334a94a7
zmfl1230 <zmfl123097@gmail.com>
2022-01-05 21:06:38 +0900

    feat: develop to join function

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,9 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class MemberController {
@@ -11,5 +14,16 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("members/create")
+    public String createIndex() {
+        return "members/create-index";
+    }
+
+    @PostMapping("members/create")
+    public String create(@RequestBody String name) {
+        memberService.join(name);
+        return "redirect:/";
     }
 }
