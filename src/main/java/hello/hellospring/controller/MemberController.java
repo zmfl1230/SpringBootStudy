b4789ce4014bf5b282a46baaa995b94da960bc48
zmfl1230 <zmfl123097@gmail.com>
2022-01-05 21:13:55 +0900

    feat: develop function to get list of members

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,11 +1,15 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -26,4 +30,12 @@ public class MemberController {
         memberService.join(name);
         return "redirect:/";
     }
+
+    @GetMapping("members")
+    public String memberList(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/list";
+    }
+
 }
