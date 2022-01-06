51269c30862cb1b875599c760e14022bbda2dde5
zmfl1230 <zmfl123097@gmail.com>
2022-01-06 21:44:31 +0900

    refactor: remove auto bean registration annotations, register bean with java code

--- src/main/java/hello/hellospring/factory/MemberFactory.java
+++ src/main/java/hello/hellospring/factory/MemberFactory.java
@@ -1,10 +1,23 @@
 package hello.hellospring.factory;
 
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MemberFactory {
+
+    @Bean
     public MemberService memberService() {
         return new MemberService(new MemoryMemberRepository());
     }
+
+//
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 }
