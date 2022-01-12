ff2ef322f195806283d9007534545e389be06db9
zmfl1230 <zmfl123097@gmail.com>
2022-01-13 02:05:49 +0900

    feat: [회원 주문 관리/할인 정책 적용] (아직 미정인) 환불 정책 정의 인터페이스를 구현한 정액 할인 정책, 정률 할인 정책 클래스 생성

---
+++ src/main/java/hello/hellospring/repository/order/RateDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.hellospring.repository.order;
+
+import hello.hellospring.domain.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+    @Override
+    public void validateMemberGrade(Member member) {
+
+    }
+
+    @Override
+    public void validatePurchasePrice(int price) {
+
+    }
+
+    @Override
+    public int discount(int price) {
+        return 0;
+    }
+}
