69bb4aeac0ad1d8e5baecf211abeb4f35fc16d00
zmfl1230 <zmfl123097@gmail.com>
2022-01-13 13:59:33 +0900

    feat: [회원 주문 관리/할인 정책 적용] exception message 생성

---
+++ src/main/java/hello/hellospring/exception/ExceptionMessage.java
@@ -0,0 +1,6 @@
+package hello.hellospring.exception;
+
+public class ExceptionMessage {
+    public static final String UNAUTHORIZED_GRADE = "권한이 없는 등급입니다.";
+    public static final String NOT_ENOUGH_PRICE_FOR_DISCOUNT = "가격이 충족되지 않았습니다.";
+}
