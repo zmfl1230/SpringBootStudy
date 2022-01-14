26abb5fc10c6fd738f93986630680bb143458f8d
zmfl1230 <zmfl123097@gmail.com>
2022-01-14 18:30:11 +0900

    style: [회원 주문 관리/할인 정책 적용] OrderFactory 설정 파일에 역할-구현관련 주석 추가

--- src/main/java/hello/hellospring/factory/OrderFactory.java
+++ src/main/java/hello/hellospring/factory/OrderFactory.java
@@ -12,7 +12,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class OrderFactory {
-    @Autowired DataSource dataSource;
+    @Autowired  DataSource dataSource;
 
     @Bean
     public OrderService orderService() {
@@ -20,13 +20,21 @@ public class OrderFactory {
     }
 
     @Bean
+    /*
+    역할이라 함은
+    역할은 추상적인 것으로써 어떤 로미오라는 역할이 있으나 구체적으로 어떤 배우가 이 로미오의 역을 맡아 연기를 해줄지에 대해서는 아직 모르는 것.
+    이 설정 클래스에선 이 각각의 역할에 구체적인 구현체를 생성해 인자로 넣어준다.
+    * */
     public DiscountPolicy discountPolicy() {
-    // 언제든 변경 가능한 할인 정책에 대해 유연하게 대응하기 위함
+        // 언제든 변경 가능한 할인 정책에 대해 유연하게 대응하기 위체
+        // 구체적인 구현
         return new FixDiscountPolicy();
     }
 
     @Bean
+    // 역할
     public OrderRepository orderRepository() {
+        // 구체적인 구현체
         return new JdbcOrderRepository(dataSource);
     }
 
