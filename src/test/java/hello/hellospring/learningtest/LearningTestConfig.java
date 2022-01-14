da57479eafdab3a3f025ba6274c1a1a99f0aa7a3
zmfl1230 <zmfl123097@gmail.com>
2022-01-14 18:30:59 +0900

    feat: [학습 테스트] 학습 테스트를 위한 테스트 의존관계 설정 파일 생성

---
+++ src/test/java/hello/hellospring/learningtest/LearningTestConfig.java
@@ -0,0 +1,36 @@
+package hello.hellospring.learningtest;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.order.DiscountPolicy;
+import hello.hellospring.repository.order.FixDiscountPolicy;
+import hello.hellospring.repository.order.RateDiscountPolicy;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LearningTestConfig {
+
+    @Bean
+    public DiscountPolicy fixDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
+    @Bean
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
+
