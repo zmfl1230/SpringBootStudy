e2914477b83379453f5085ad790b7f19d9e4e8c2
zmfl1230 <zmfl123097@gmail.com>
2022-01-16 17:53:59 +0900

    feat: [@Configuration과 싱글톤과의 관계 테스트] 테스트를 위한 MemberRepository를 반환하는 코드 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -53,4 +53,7 @@ public class MemberService {
     public void deleteAll() {
         memberRepository.deleteAll();
     }
+
+    // 테스트를 위한 코드 추가
+    public MemberRepository getMemberRepository() { return memberRepository; }
 }
