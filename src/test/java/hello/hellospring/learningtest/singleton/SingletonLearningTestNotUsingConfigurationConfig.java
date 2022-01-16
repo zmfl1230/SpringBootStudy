2b2425e5cdb6e5477b698547ad9697907f32b826
zmfl1230 <zmfl123097@gmail.com>
2022-01-16 17:52:12 +0900

    feat: [@Configuration과 싱글톤과의 관계 테스트] @Confifuration이 붙지 않은 의존관계 클래스 생성

---
+++ src/test/java/hello/hellospring/learningtest/singleton/SingletonLearningTestNotUsingConfigurationConfig.java
@@ -0,0 +1,23 @@
+package hello.hellospring.learningtest.singleton;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+
+public class SingletonLearningTestNotUsingConfigurationConfig {
+
+    @Bean
+    public StatefulService statefulService() { return new StatefulService(); }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
