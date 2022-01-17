e5cff937708e5e3cc384b17ece61cb130088e7a0
zmfl1230 <zmfl123097@gmail.com>
2022-01-17 17:50:17 +0900

    feat: [자동 주입 시, 지정한 타입과 관련된 복수의 빈 오브젝트가 존재할 때 해결방법] @Primary와 @Qualifier를 각각의 의존관계 설정 메서드에 붙임

--- src/main/java/hello/hellospring/factory/OrderFactory.java
+++ src/main/java/hello/hellospring/factory/OrderFactory.java
@@ -1,13 +1,13 @@
 package hello.hellospring.factory;
 
-import hello.hellospring.repository.order.DiscountPolicy;
-import hello.hellospring.repository.order.FixDiscountPolicy;
-import hello.hellospring.repository.order.JdbcOrderRepository;
-import hello.hellospring.repository.order.OrderRepository;
+import hello.hellospring.repository.order.*;
 import hello.hellospring.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -25,10 +25,19 @@ public class OrderFactory {
     역할은 추상적인 것으로써 어떤 로미오라는 역할이 있으나 구체적으로 어떤 배우가 이 로미오의 역을 맡아 연기를 해줄지에 대해서는 아직 모르는 것.
     이 설정 클래스에선 이 각각의 역할에 구체적인 구현체를 생성해 인자로 넣어준다.
     * */
+
+    @Primary
+    @Qualifier("fixDiscountPolicy")
     public DiscountPolicy discountPolicy() {
         // 언제든 변경 가능한 할인 정책에 대해 유연하게 대응하기 위체
         // 구체적인 구현
         return new FixDiscountPolicy();
+    }
+
+    @Bean
+    @Qualifier("rateDiscountPolicy")
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 
     @Bean
