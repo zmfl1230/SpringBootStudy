5c7b56a01ce2244f5b92f86a2e3fc17b73fe066c
zmfl1230 <zmfl123097@gmail.com>
2022-01-18 13:48:26 +0900

    fix: [다형성 활용] 다형성을 활용 테스트 과정에서 발견한 return 값 버그 수정

--- src/main/java/hello/hellospring/repository/order/FixDiscountPolicy.java
+++ src/main/java/hello/hellospring/repository/order/FixDiscountPolicy.java
@@ -3,10 +3,10 @@ package hello.hellospring.repository.order;
 import hello.hellospring.CommonConstant;
 
 public class FixDiscountPolicy implements DiscountPolicy {
-    private final int PAYMENT_AMOUNT_ON_FIX_DISCOUNT = CommonConstant.STANDARD_PRICE - CommonConstant.FIX_DISCOUNT;
+    private final int PAYMENT_AMOUNT_ON_FIX_DISCOUNT = CommonConstant.FIX_DISCOUNT;
 
     @Override
     public int discount(int price) {
-        return PAYMENT_AMOUNT_ON_FIX_DISCOUNT;
+        return price - PAYMENT_AMOUNT_ON_FIX_DISCOUNT;
     }
 }
--- src/main/java/hello/hellospring/repository/order/RateDiscountPolicy.java
+++ src/main/java/hello/hellospring/repository/order/RateDiscountPolicy.java
@@ -4,9 +4,9 @@ import hello.hellospring.CommonConstant;
 import hello.hellospring.domain.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy {
-    private final float PAYMENT_AMOUNT_ON_RATE_DISCOUNT = CommonConstant.STANDARD_PRICE * (1 - CommonConstant.RATE_DISCOUNT);
+    private final float PAYMENT_AMOUNT_ON_RATE_DISCOUNT = (1 - CommonConstant.RATE_DISCOUNT);
     @Override
     public int discount(int price) {
-        return (int) PAYMENT_AMOUNT_ON_RATE_DISCOUNT;
+        return (int) (price * PAYMENT_AMOUNT_ON_RATE_DISCOUNT);
     }
 }
